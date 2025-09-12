365668eda0506d094cc4f855a51c29eb35372b52
Pablo Martin Gore <pablo.gore@adalove.com.ar>
2025-09-12 15:50:18 -0300

    Simplify WithNewFile call in HTTPS cloner
    - Remove unnecessary options struct for .netrc file creation
    - Use direct content parameter instead of ContainerWithNewFileOpts

--- internal/pipelines/shared/https_cloner.go
+++ internal/pipelines/shared/https_cloner.go
@@ -97,11 +97,7 @@ func (c *HTTPSCloner) Clone(ctx context.Context, client *dagger.Client, opts Git
 		netrc := fmt.Sprintf("machine gitlab.com login %s password %s", creds.User, creds.Token)
 		container = container.
 			WithEnvVariable("HOME", "/root").
-			WithNewFile("/root/.netrc", dagger.ContainerWithNewFileOpts{
-				Contents:    netrc,
-				Permissions: 0o600,
-				Owner:       "root",
-			}).
+			WithNewFile("/root/.netrc", netrc).
 			WithExec([]string{"chmod", "600", "/root/.netrc"}).
 			WithExec([]string{"git", "config", "--global", "credential.helper", "store"}).
 			WithExec([]string{"git", "config", "--global", "http.sslVerify", "false"})
